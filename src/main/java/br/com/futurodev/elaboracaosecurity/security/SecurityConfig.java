f9264640c994f82e6818cd754be708a842f4e39b
Denilson Muller <denis@gmail.com>
2022-10-28 15:19:23 -0300

    primeiro commit

--- src/main/java/br/com/futurodev/elaboracaosecurity/security/SecurityConfig.java
+++ src/main/java/br/com/futurodev/elaboracaosecurity/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll() // endpoint "login" liberado à todos
+                .antMatchers("/login").permitAll() // endpoint "login" liberado a todos
                 .antMatchers("/pets").hasAnyAuthority("PETS", "ADMIN") // Liberado somente para usuários com a devida autorização
                 .antMatchers("/tutores").hasAnyAuthority("TUTORES", "ADMIN") // Liberado somente para usuários com a devida autorização
                 .anyRequest().authenticated() // Todos os demais endpoints precisam de autentição
